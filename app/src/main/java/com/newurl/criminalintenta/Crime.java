b7d4759b38f5e70bcac8c6904cd8dfd1f5b18096
Thaddeus <anoanony99@hushmail.com>
2016-04-20 18:00:47 -0400

    Ch7

---
+++ app/src/main/java/com/newurl/criminalintenta/Crime.java
@@ -0,0 +1,17 @@
+package com.newurl.criminalintenta;
+
+import java.util.UUID;
+
+/**
+ * Created by a on 4/20/2016.
+ */
+public class Crime {
+    private UUID mId; private String mTitle;
+
+    public Crime(){
+        mId=UUID.randomUUID();
+    }
+
+    
+
+}
